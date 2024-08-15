3db43725185b12aa8fbf52c54db5c82b515e3c35
khanjandave <khanjancentennial@gmail.com>
2024-08-14 23:37:12 -0400

    api call all patients

--- src/Components/Patients/patients.js
+++ src/Components/Patients/patients.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import './patients.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Patients() {
-    // Example data for the table
-  const data = [
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', phone: '123-456-7890', gender: 'Male' },
-    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', phone: '123-456-7891', gender: 'Female' },
-    { firstName: 'Jim', lastName: 'Beam', email: 'jim.beam@example.com', phone: '123-456-7892', gender: 'Male' },
-    { firstName: 'Jack', lastName: 'Daniels', email: 'jack.daniels@example.com', phone: '123-456-7893', gender: 'Male' },
-    { firstName: 'Jill', lastName: 'Valentine', email: 'jill.valentine@example.com', phone: '123-456-7894', gender: 'Female' },
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', phone: '123-456-7890', gender: 'Male' },
-    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', phone: '123-456-7891', gender: 'Female' },
-    { firstName: 'Jim', lastName: 'Beam', email: 'jim.beam@example.com', phone: '123-456-7892', gender: 'Male' },
-    { firstName: 'Jack', lastName: 'Daniels', email: 'jack.daniels@example.com', phone: '123-456-7893', gender: 'Male' },
-    { firstName: 'Jill', lastName: 'Valentine', email: 'jill.valentine@example.com', phone: '123-456-7894', gender: 'Female' },
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', phone: '123-456-7890', gender: 'Male' },
-    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', phone: '123-456-7891', gender: 'Female' },
-    { firstName: 'Jim', lastName: 'Beam', email: 'jim.beam@example.com', phone: '123-456-7892', gender: 'Male' },
-    { firstName: 'Jack', lastName: 'Daniels', email: 'jack.daniels@example.com', phone: '123-456-7893', gender: 'Male' },
-    { firstName: 'Jill', lastName: 'Valentine', email: 'jill.valentine@example.com', phone: '123-456-7894', gender: 'Female' },
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', phone: '123-456-7890', gender: 'Male' },
-    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', phone: '123-456-7891', gender: 'Female' },
-    { firstName: 'Jim', lastName: 'Beam', email: 'jim.beam@example.com', phone: '123-456-7892', gender: 'Male' },
-    { firstName: 'Jack', lastName: 'Daniels', email: 'jack.daniels@example.com', phone: '123-456-7893', gender: 'Male' },
-    { firstName: 'Jill', lastName: 'Valentine', email: 'jill.valentine@example.com', phone: '123-456-7894', gender: 'Female' },
-    { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', phone: '123-456-7890', gender: 'Male' },
-    { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', phone: '123-456-7891', gender: 'Female' },
-    { firstName: 'Jim', lastName: 'Beam', email: 'jim.beam@example.com', phone: '123-456-7892', gender: 'Male' },
-    { firstName: 'Jack', lastName: 'Daniels', email: 'jack.daniels@example.com', phone: '123-456-7893', gender: 'Male' },
-    { firstName: 'Jill', lastName: 'Valentine', email: 'jill.valentine@example.com', phone: '123-456-7894', gender: 'Female' },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://group3-mapd713.onrender.com/patient/list')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);  
 
   const handleEdit = (index) => {
     console.log('Edit item at index:', index);
@@ -50,26 +32,48 @@ function Patients() {
             <th>Email Id</th>
             <th>Phone Number</th>
             <th>Gender</th>
+            <th>Weight</th>
+            <th>Height</th>
+            <th>Address</th>
+            <th>Status</th>
             <th>Edit</th>
             <th>Delete</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
+          {
+          data.length > 0 ? (
+          data.map((item, index) => (
             <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
               <td>{item.firstName}</td>
               <td>{item.lastName}</td>
               <td>{item.email}</td>
-              <td>{item.phone}</td>
-              <td>{item.gender}</td>
+              <td>{item.phoneNumber}</td>
+              <td>{item.gender === 0 ? "Male" : "Female"}</td>
+              <td>{item.weight}</td>
+              <td>{item.height}</td>
+              <td>{item.address}</td>
+              <td>{item.status}</td>
               <td>
-                <button onClick={() => handleEdit(index)}>Edit</button>
-              </td>
-              <td>
-                <button onClick={() => handleDelete(index)}>Delete</button>
-              </td>
+                    <button onClick={() => handleEdit(index)}>
+                      <FontAwesomeIcon icon={faEdit} />
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => handleDelete(index)}>
+                      <FontAwesomeIcon icon={faTrashAlt} />
+                    </button>
+                  </td>
             </tr>
-          ))}
+          )))
+          :
+          (
+            <tr>
+              <td colSpan="7">Loading data...</td>
+            </tr>
+          )
+        
+        }
         </tbody>
       </table>
     </div>
---
+++ src/Model/Patients.js
@@ -0,0 +1,16 @@
+class Patients {
+    constructor({ _id, firstName, lastName, email, phoneNumber, gender, weight, height, address, status }) {
+      this.id = _id;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.phoneNumber = phoneNumber;
+      this.gender = gender;
+      this.weight = weight;
+      this.height = height;
+      this.address = address;
+      this.status = status
+    }
+  }
+  
+  export default User;
